5c6a31531dfebc326c359e7d4a0bf786f9b91dad
AStolln <94230359+AStolln@users.noreply.github.com>
2021-11-18 15:48:37 +0100

    Add files via upload
    all changes to the two files are marked with "ASTRID" and commented

--- Geoquiz/script.js
+++ Geoquiz/script.js
@@ -50,7 +50,41 @@ function initMap()
 		}
 	);
 	
+	//ASTRID-----------------------------------------------------
+	//load the first question
+	load_question();
+	//-----------------------------------------------------
+	
 }
+
+//ASTRID-----------------------------------------------------
+//questions
+function load_question(){
+	//console.log(JSON.parse(questionsJSONString));
+	var quiz_questions = JSON.parse(questionsJSONString);
+	
+	var questionNumber = get_randomNumber(quiz_questions.Questions.length);
+	var question = quiz_questions.Questions[questionNumber];
+	
+	//console.log(question.question);
+	
+	document.getElementById("myQuestion").innerHTML = question.question;
+	document.getElementById("myAnswer").innerHTML = question.answer;
+}
+
+//get a random number to choose the next question
+function get_randomNumber(n){
+	randomNumber= Math.floor(Math.random() * (n));
+	return randomNumber;
+}
+//TO DO: 
+// 1)a question should only come once per game --> 
+// memorize ID of questions, which have already been asked and exclude them from possible next questions
+// 2) load new question after one is answered
+// 3) add more questions to the questions.json --> what attributes are needed for evaluation
+//To DISCUSS:
+// Where do questions come from? JSON-file as suggested? 
+//-----------------------------------------------------
 
 //Main entry point
 initMap();
