cef95b54b52b335e6253f3b301a41aefa045ce63
Henrike21 <58042175+Henrike21@users.noreply.github.com>
2021-11-23 20:08:25 +0100

    questions in array format

---
+++ questions.js
@@ -0,0 +1,39 @@
+
+
+var data =
+[
+
+	{
+		question: "What historic city is located between two seas and is split between two continents?",
+		correctAnswer: "Istanbul" 
+	},
+
+	{
+		question: "What is the most populated urban place?",
+		correctAnswer: "Tokyo"
+	},
+]
+/*
+"3"	
+	{
+		question: "Most of the population within the limits of this major American city live on islands. Where is this city?",
+		correcAnswer: "New York"
+	},
+"4"
+	{
+		question: "Which river crosses the equator twice? ",
+		correcAnswer: "Congo"
+	},
+"5"
+	
+	{
+		question: "In which country is ice hockey the national sport?",
+		correcAnswer: "Canada"
+	},
+"6"
+	{
+		question: "What is the most populated part of the world?  ",
+		correcAnswer: "Europe"
+	},
+}*/
+
