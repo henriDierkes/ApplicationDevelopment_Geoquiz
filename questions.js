80969943d5ec0d7ebfbed13178541297ac7c2929
AStolln <94230359+AStolln@users.noreply.github.com>
2021-11-24 10:30:14 +0100

    worked on questions
    a few changes in script.js and questions.js

--- questions.js
+++ questions.js
@@ -1,19 +1,70 @@
+var data = [{
+	"question": "What historic city is located between two seas and is split between two continents?",
+	"correctAnswer": "Istanbul",
+	"question_id": 1,
+	"category":"city"
+}, {
+	"question": "What is the most populated urban place?",
+	"correcAnswer": "Tokio",
+	"question_id": 2,
+	"category":"city"
+}, {
+	"question":"Most of the population within the limits of this major American city live on islands. Where is this city?",
+	"correcAnswer":"New York",
+	"question_id":3,
+	"category":"city"
+}, {
+	"question":"What is the most populous city in South America?",
+	"correcAnswer":"São Paulo",
+	"question_id":4,
+	"category":"city"
+}, {
+	"question":"Home of the famous Eifel Tower?",
+	"correcAnswer":"Paris",
+	"question_id":5,
+	"category":"city"
+}, {
+	"question":"Which Asian city is also an independent nation?",
+	"correcAnswer":"Singapore",
+	"question_id":6,
+	"category":"city"
+}, {
+	"question":"Which city in the world has the most bridges?",
+	"correcAnswer":"Hamburg",
+	"question_id":7,
+	"category":"city"
+}, {
+	"question":"Which city was built on 7 hills?",
+	"correcAnswer":"Rome",
+	"question_id":8,
+	"category":"city"
+}, {
+	"question":"What is the capital city of Litvania?",
+	"correcAnswer":"Vilnius",
+	"question_id":9,
+	"category":"city"
+}, {
+	"question":"Which city is famous for one of the Seven Wonders of the World and its Great Library in ancient times?",
+	"correcAnswer":"Alexandria",
+	"question_id":10,
+	"category":"city"
+}
 
+]
 
-var data =
-[
-
+/*{
+"1":
 	{
-		question: "What historic city is located between two seas and is split between two continents?",
-		correctAnswer: "Istanbul" 
-	},
-
+		"question" : ["What historic city is located between two seas and is split between two continents?"],
+		"correcAnswer" : ["Istanbul"]
+	}
+}*/
+/*
+"2"	
 	{
 		question: "What is the most populated urban place?",
-		correctAnswer: "Tokyo"
+		correcAnswer: "Tokio"
 	},
-]
-/*
 "3"	
 	{
 		question: "Most of the population within the limits of this major American city live on islands. Where is this city?",
@@ -36,4 +87,3 @@ var data =
 		correcAnswer: "Europe"
 	},
 }*/
-
--- script.js
+++ script.js
@@ -9,6 +9,11 @@ var map;
 var markerGroup;
 var markerGroupCorrect;
 
+//global variables to limit number of questions per game
+//and to store id´s of already used questions per game to avoid asking a question twice
+var numberOfQuestions = 0;
+var usedQuestions = [];
+
 
 // Functions to add OpenLayers map
 
@@ -50,7 +55,7 @@ function init()
 	markerGroupCorrect = L.layerGroup();
 	map.addLayer(markerGroupCorrect);
 	
-	
+	loadQuestion();//load first question when page is loaded
 	
 	
 }
@@ -58,13 +63,13 @@ function init()
 // Load questions 
 function loadQuestion()
 { //myQuestion
-	for(var i = 0; i < data.length; i++){
+	//for(var i = 0; i < data.length; i++){
 		//console.log(data[i].question)
 		//console.log(document.getElementById("answer").value)
 		
 		//another function for this down to be evaluated
 		//prints out the question to log
-		document.getElementById("myQuestion").innerHTML = data[i].question
+	//	document.getElementById("myQuestion").innerHTML = data[i].question
 		/*
 		var givenQuestion = data[i].correctAnswer
 		var givenAnswer = document.getElementById("answer").value
@@ -74,12 +79,30 @@ function loadQuestion()
 			console.log("NO")
 		}*/
 	
+	//}
+	console.info(data);
+	
+	//pick a random question from the list of questions in questions.js	
+	//and check, that this question is not used already in the game 
+	do{
+		//get random question
+		var questions = data[Math.floor(Math.random()*data.length)];
+		console.log("In do-loop " + questions.question_id);
 	}
-	// console.info(data);
+	//as long as it was not already asked in this game
+	while(usedQuestions.includes(questions.question_id))
 	
-	// var questions = data[Math.floor(Math.random()*data.length)];
+	//and display it in the left canvas
+	document.getElementById("myQuestion").innerHTML = questions["question"];
 	
-	// document.getElementById("myQuestion").innerHTML = questions["questions"];
+	//increase the counter by one
+	numberOfQuestions++;
+	//store the id of the used question in an Array
+	usedQuestions.push(questions.question_id);
+	
+	console.log("Number of questions " + numberOfQuestions);
+	console.log("Used Questions " + usedQuestions);
+	
 }
 
 
@@ -112,7 +135,22 @@ function searchResponse( data )
 {
 	var latlon = [];
 	var answer = [];
-	loadQuestion();
+	
+	//after giving an answer load next question --> clicking on Go-Button at the moment
+	//or when answer was checked
+	//############# have to decide, when the next question should be loaded!!!!!!#####
+	if (numberOfQuestions >= 5){
+		//after 5 questions the game is over and the results should be displayed!
+		//max. number of questions per game can be changed of course
+		console.log("End of Game!");
+		//set counter and array for question control back to zero
+		numberOfQuestions = 0;
+		usedQuestions = [];
+	}
+	else{
+		loadQuestion(); 
+	}
+	
 	// Loop Result Location
 	for (var i= 0; i< data.features.length; i++)
 	{
