fccbb6572809af105edd6213202857531215daeb
Henrike21 <58042175+Henrike21@users.noreply.github.com>
2021-11-25 22:32:16 +0100

    Final version of start page & main Quiz
    adjusted colors and fonts for the main page. added also a score field without values so far

---
+++ GeoQuiz/questions.js
@@ -0,0 +1,53 @@
+var data = [{
+	"question": "What historic city is located between two seas and is split between two continents?",
+	"correctAnswer": "Istanbul",
+	"question_id": 1,
+	"category":"city"
+}, {
+	"question": "What is the most populated urban place?",
+	"correctAnswer": "Tokyo",
+	"question_id": 2,
+	"category":"city"
+}, {
+	"question":"Most of the population within the limits of this major American city live on islands. Where is this city?",
+	"correctAnswer":"New York",
+	"question_id":3,
+	"category":"city"
+}, {
+	"question":"What is the most populated city in South America?",
+	"correctAnswer":"Sao Paulo",
+	"question_id":4,
+	"category":"city"
+}, {
+	"question":"Home of the famous Eifel Tower?",
+	"correctAnswer":"Paris",
+	"question_id":5,
+	"category":"city"
+}, {
+	"question":"Which Asian city is also an independent nation?",
+	"correctAnswer":"Singapore",
+	"question_id":6,
+	"category":"city"
+}, {
+	"question":"Which city in the world has the most bridges?",
+	"correctAnswer":"Hamburg",
+	"question_id":7,
+	"category":"city"
+}, {
+	"question":"Which city was built on 7 hills?",
+	"correctAnswer":"Rome",
+	"question_id":8,
+	"category":"city"
+}, {
+	"question":"What is the capital city of Litvania?",
+	"correctAnswer":"Vilnius",
+	"question_id":9,
+	"category":"city"
+}, {
+	"question":"Which city is famous for one of the Seven Wonders of the World and its Great Library in ancient times?",
+	"correctAnswer":"Alexandria",
+	"question_id":10,
+	"category":"city"
+}
+
+]
